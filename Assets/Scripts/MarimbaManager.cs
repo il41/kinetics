b23f0fc991fab554ddb44687741ebd08193464d2
il41 <ilai.gilbert@gmail.com>
2019-04-23 18:44:42 -0500

    marimba stuff

--- Assets/Scripts/MarimbaManager.cs
+++ Assets/Scripts/MarimbaManager.cs
@@ -6,8 +6,10 @@ public class MarimbaManager : MonoBehaviour
 {
     public GameObject hit2;
     public int amount;
+    public int height = 2;
     public GameObject[] cubes;
     public List<Vector3> positions = new List<Vector3>();
+
 
 
     // Start is called before the first frame update
@@ -15,17 +17,19 @@ public class MarimbaManager : MonoBehaviour
     {
         for(int i = 0; i < amount; i++)
         {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //string indexString = i.ToString();
+            //cube.name = "cube"+ indexString;
+            //cube.transform.position = new Vector3(i, 2, i);
+            //cube.GetComponent<Renderer>().material.color = Color.red;
             string indexString = i.ToString();
+            var cube = Instantiate(hit2, new Vector3(i/5f, height, i/4f), Quaternion.identity);
             cube.name = "cube"+ indexString;
-            cube.transform.position = new Vector3(i, 2, i);
-            // color is controlled like this
-            cube.GetComponent<Renderer>().material.color = Color.red;
-
-            Instantiate(hit2, new Vector3(i, 1, i), Quaternion.identity);
             AudioSource hitAudio = hit2.GetComponent<AudioSource>();
             hitAudio.pitch = (i / 2.5f * i / 2.5f);
             hitAudio.playOnAwake = false;
+            cube.transform.localScale += new Vector3(i*0.2f, 0, 0);
+
 
         }
     }
@@ -33,6 +37,6 @@ public class MarimbaManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/marimbaHit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marimbaHit : MonoBehaviour
+{
+    AudioSource audioData;
+    float pitch;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        audioData = GetComponent<AudioSource>();
+        audioData.clip = Resources.Load<AudioClip>("vibe541");
+        audioData.playOnAwake = false;
+        Debug.Log(GetComponent<Collider>().bounds.size.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<AudioSource>().pitch = GetComponent<Collider>().bounds.size.x;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        audioData.Play();
+    }
+}
